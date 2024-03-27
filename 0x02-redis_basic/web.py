4b1467f448cd902ee6ddcd9b5f81722b251e7b3b
mddon <mmrealworld@yahoo.com>
2024-03-27 14:35:12 -0700

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,19 +3,21 @@
     obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-r = redis.Redis()
-count = 0
+
 
 
 def get_page(url: str) -> str:
     """ track how many times a particular URL was accessed in the key
         "count:{url}"
         and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
+
+    redis_conn = redis.Redis()
+    count = 0
+    redis_conn.set(f"cached:{url}", count)
+    response = requests.get(url)
+    redis_conn.incr(f"count:{url}")
+    redis_conn.setex(f"cached:{url}", 10, redis_conn.get(f"cached:{url}"))
+    return response.text
 
 
 if __name__ == "__main__":
